be555f9b7570805e1e0c9423a26cf135574b073d
Abhinav Bahiya <abhinav.bahiya@weboniselab.com>
2016-12-28 12:27:57 +0530

    Improved UI

--- app/src/main/java/com/example/android/subscription/MainActivity.java
+++ app/src/main/java/com/example/android/subscription/MainActivity.java
@@ -1,12 +1,9 @@
-package com.example.android.justjava;
+package com.example.android.subscription;
 
-import android.icu.text.NumberFormat;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
-
-import com.example.android.subscription.R;
 
 public class MainActivity extends AppCompatActivity {
     int quantity = 1;
@@ -17,21 +14,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void submitOrder(View view) {
-        displayPrice(quantity * 10);
-    }
 
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_textView);
         quantityTextView.setText("" + number);
-        displayPrice(number * 10);
 
     }
 
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_textView);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-    }
 
     public void increment(View view) {
         quantity = quantity + 1;
